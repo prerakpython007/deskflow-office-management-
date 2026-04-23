e87293804e4f825052a4b9c65d7656d0e4854afe
prerakpython007 <prerak.siddhpura@gmail.com>
2026-04-23 23:23:53 +0530

    finished doing the company and employee model setup

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    public function employees()
+    {
+        return $this -> hasMany(Employee::class);
+    }
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+   protected $fillable = [
+    'name',
+    'email',
+    'position',
+    'company_id',
+    'manager_id',
+    'country',
+    'state',
+    'city'
+];
+
+public function company()
+{
+    return $this->belongsTo(Company::class);
+}
+
+public function manager()
+{
+    return $this->belongsTo(Employee::class, 'manager_id');
+}
+
+public function subordinates()
+{
+    return $this->hasMany(Employee::class, 'manager_id');
+}
 }
